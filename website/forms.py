b0b3dcbb93e1282265bbeb60e788781ecd674e43
nlutala <nathanlutala@outlook.com>
2024-06-10 15:49:46 +0100

    Update forms.py, pdf_merger to use input to handle more than one file inputted by user and updated requirements.txt with installation of MultiFileField class

--- website/forms.py
+++ website/forms.py
@@ -1,7 +1,8 @@
 from django import forms
+from multiupload.fields import MultiFileField
 
 
 class PDFMergerForm(forms.Form):
-    formFileMultiple = forms.FileField()
-    mergedFileName = forms.CharField(label="Merged file name")
+    formFileMultiple = MultiFileField(label="Attachments", min_num=2)
+    mergedFileName = forms.CharField(label="Merged file name", required=False)
     
