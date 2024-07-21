08a4da536d63c457750b5cffbcc1337455b2d953
nlutala <nathanlutala@outlook.com>
2024-07-21 08:56:15 +0100

    Update pdf_merger error messages and give feedback to the website if only one or no pdf files are submitted

--- website/views.py
+++ website/views.py
@@ -5,57 +5,66 @@ import os
 
 # Create your views here.
 
+
 def index(request):
-    '''
+    """
     Returns the homepage of the website.
-    '''
+    """
     return render(request, "website/index.html")
 
+
 def projects(request):
-    '''
+    """
     Returns a page with a list of projects to try out.
-    '''
+    """
     return render(request, "website/projects.html")
 
+
 def pdf_merger(request):
-    '''
+    """
     Returns a page with a the PDF Merger project to try out.
-    '''
+    """
     merger = PDFMerger()
     BASE_DIR = merger.get_base_dir()
-    
+
     if request.method == "POST":
-        pdfs = [file.name for file in request.FILES.getlist('formFileMultiple')]
-        
-        if len(pdfs) < 2:
-            return render(request, "website/pdf_merger.html")
-        
-        for file in request.FILES.getlist('formFileMultiple'):
+        pdfs = [file.name for file in request.FILES.getlist("formFileMultiple")]
+
+        for file in request.FILES.getlist("formFileMultiple"):
             file_path = os.path.join(BASE_DIR, file.name)
-            with open(file_path, 'wb+') as new_file:
+            with open(file_path, "wb+") as new_file:
                 for chunk in file.chunks():
                     new_file.write(chunk)
-        
-        merger.add_pdf_files(pdfs)
-        filename = merger.merge_pdfs(request.POST.get('mergedFileName'))
-        
+
+        try:
+            merger.add_pdf_files(pdfs)
+            filename = merger.merge_pdfs(request.POST.get("mergedFileName"))
+        except ValueError as error_message:
+            for file in os.listdir(BASE_DIR):
+                if file.endswith(".pdf"):
+                    os.remove(os.path.join(BASE_DIR, file))
+            return render(
+                request, "website/pdf_merger.html", {"error_message": error_message}
+            )
+
         if filename.endswith(".pdf"):
-            pdf = open(filename, 'rb')
+            pdf = open(filename, "rb")
         else:
-            pdf = open(f'{filename}.pdf', 'rb')
-            
-        response = FileResponse(pdf, content_type='application/pdf')
-        response['Content-Disposition'] = f'inline; filename="{filename}"'
+            pdf = open(f"{filename}.pdf", "rb")
+
+        response = FileResponse(pdf, content_type="application/pdf")
+        response["Content-Disposition"] = f'inline; filename="{filename}"'
         return response
     else:
         for file in os.listdir(BASE_DIR):
             if file.endswith(".pdf"):
                 os.remove(os.path.join(BASE_DIR, file))
-        
+
     return render(request, "website/pdf_merger.html")
 
+
 def contact(request):
-    '''
+    """
     Returns a page with where to contact/follow me (Github & LinkedIn).
-    '''
+    """
     return render(request, "website/contact.html")
